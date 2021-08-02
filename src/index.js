000a37345048c8f4d7a011e175b0d641cb528ade
s-haya-123 <ganbaru.wakenai@gmail.com>
2021-08-02 19:53:06 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import * as HoloPlay from '../node_modules/holoplay/dist/holoplay.module.js';
+const scene = new THREE.Scene();
+const loader = new THREE.GLTFLoader();
+loader.load(
 
-import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { VRM } from '../node_modules/@pixiv/three-vrm';
-console.log(HoloPlay);
-console.log(THREE);
-console.log(VRM);
-console.log(GLTFLoader);
+	// URL of the VRM you want to load
+	'/models/RearAlice.vrm',
+
+	// called when the resource is loaded
+	( gltf ) => {
+
+		// generate a VRM instance from gltf
+		THREE.VRM.from( gltf ).then( ( vrm ) => {
+
+			// add the loaded vrm to the scene
+			scene.add( vrm.scene );
+
+			// deal with vrm features
+			console.log( vrm );
+
+		} );
+
+	},
+
+	// called while loading is progressing
+	( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
+
+	// called when loading has errors
+	( error ) => console.error( error )
+
+);
