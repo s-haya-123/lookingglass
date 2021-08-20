e05759e2778fd3ec7a61b644f5e1e488ba53d930
s-haya-123 <ganbaru.wakenai@gmail.com>
2021-08-20 18:16:35 +0900

    feat: js vrm sample

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
+document.body.appendChild( renderer.domElement );
+
+// camera
+const camera = new THREE.PerspectiveCamera( 30.0, window.innerWidth / window.innerHeight, 0.1, 20.0 );
+camera.position.set( 0.0, 1.0, 5.0 );
+
+// camera controls
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.screenSpacePanning = true;
+controls.target.set( 0.0, 1.0, 0.0 );
+controls.update();
+
+// scene
 const scene = new THREE.Scene();
+
+// light
+const light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 1.0, 1.0, 1.0 ).normalize();
+scene.add( light );
+
+// gltf and vrm
 const loader = new THREE.GLTFLoader();
+loader.crossOrigin = 'anonymous';
 loader.load(
 
 	// URL of the VRM you want to load
-	'/models/RearAlice.vrm',
+	'./models/RearAlice.vrm',
 
 	// called when the resource is loaded
 	( gltf ) => {
 
-		// generate a VRM instance from gltf
+		// calling this function greatly improves the performance
+		THREE.VRMUtils.removeUnnecessaryJoints( gltf.scene );
+
+		// generate VRM instance from gltf
 		THREE.VRM.from( gltf ).then( ( vrm ) => {
 
-			// add the loaded vrm to the scene
+			console.log( vrm );
 			scene.add( vrm.scene );
 
-			// deal with vrm features
-			console.log( vrm );
+			vrm.humanoid.getBoneNode( THREE.VRMSchema.HumanoidBoneName.Hips ).rotation.y = Math.PI;
 
 		} );
 
@@ -28,3 +55,20 @@ loader.load(
 	( error ) => console.error( error )
 
 );
+
+// helpers
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	renderer.render( scene, camera );
+
+}
+
+animate();
